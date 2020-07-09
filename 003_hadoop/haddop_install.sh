457bd78553facbe2642d087a5554a51bd763b038
Eric Eduardo Solis Montufar <36862712+MontufarEric@users.noreply.github.com>
2020-07-09 11:29:24 -0400

    Update haddop_install.sh

--- 003_hadoop/haddop_install.sh
+++ 003_hadoop/haddop_install.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+
+""""
+ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 0600 ~/.ssh/authorized_keys
+wget http://mirror.cc.columbia.edu/pub/software/apache/hadoop/common/hadoop-3.1.3/hadoop-3.1.3.tar.gz
+
+""""
+
+
 ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 0600 ~/.ssh/authorized_keys
